fe26ebd1dfbb7d67007c11cc99b850c7f8b3915a
Kitty <zainab_97@hotmail.co.uk>
2021-10-28 17:26:03 +0300

    create done

--- apis/events/controllers.js
+++ apis/events/controllers.js
@@ -1,6 +1,6 @@
 const Planner = require("../../db/model/Planner");
 
-const eventListFetch = async (req, res) => {
+const eventListFetch = async (req, res, next) => {
   try {
     const events = await Planner.find();
     return res.json(events);
@@ -9,4 +9,17 @@ const eventListFetch = async (req, res) => {
   }
 };
 
-module.exports = { eventListFetch };
+const eventCreate = async (req, res, next) => {
+  try {
+    const newEvent = await Planner.create(req.body);
+    res.status(201).json(newEvent);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const eventUpdate = async (req, res, next) => {
+  const { eventId } = req.params;
+};
+
+module.exports = { eventListFetch, eventCreate, eventUpdate };
--- apis/events/routes.js
+++ apis/events/routes.js
@@ -1,10 +1,20 @@
-const { Router } = require("express");
 const express = require("express");
 
-const { eventListFetch } = require("./controllers");
+const {
+  eventListFetch,
+  eventCreate,
+  eventUpdate,
+  eventDelete,
+} = require("./controllers");
 
 const router = express.Router();
 
+router.post("/", eventCreate);
+
 router.get("/", eventListFetch);
+
+router.put("/:eventId", eventUpdate);
+
+// router.delete("/:eventId", eventDelete);
 
 module.exports = router;
